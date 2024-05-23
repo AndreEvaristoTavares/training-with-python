440499a824b68cf8ac6ac5caf9ceade41ac7e6dc
andre <evaristoandre106@gmail.com>
2024-05-23 18:01:46 -0300

    adicionado a função de criar titular

--- banco.py
+++ banco.py
@@ -1,5 +1,13 @@
+import random
 saldo_total = 0
+conta = {}
+titular = {}
 extrato = []
+
+def recebe_dados(cpf, nome, endereco, conta):
+    titular[cpf] = {'nome': nome, 'endereco': endereco, 'conta':conta}
+
+
 def deposito(valor):
     global saldo_total
     try:
@@ -27,6 +35,23 @@ def exibir_extrato():
     for e in extrato:
         print(e)
     print()
+
+while True:
+    opcao = int(input("cadastro de titular aperte 1 - continuar, aperte 0 - sair:  "))
+    if opcao == 1:
+        conta = random.randint(1000, 9999)
+        recebe_dados(
+            input("digite o cpf: "),
+            input("digite o nome: "),
+            input("digite o endereço completo: "), 
+            conta
+            )
+        continue
+    elif opcao == 0:
+        print("adeus...")
+        break
+    else:
+        print('opcao invalida')
 while True:
     operacao = input('digite a operação que deseja fazer: \nD = deposito, \nS = saque, \nE = extrato, \nX = sair \n')
     if operacao == 'd':
