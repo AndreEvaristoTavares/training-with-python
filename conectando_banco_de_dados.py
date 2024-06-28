a83b26989b3ee7d8f0cb3535aeb64b7617af0984
andre <evaristoandre106@gmail.com>
2024-06-28 18:03:24 -0300

    adicionado poo

--- conectando_banco_de_dados.py
+++ conectando_banco_de_dados.py
@@ -6,74 +6,80 @@ USER = 'root'
 PASSWORD = 'root'
 DB_NAME = 'portaria'
 TABLE_NAME = 'moradores'
+class Morador:
+    def __init__(self) -> None:
+        
+        self.connection = pymysql.connect(
+            host=HOST,
+            port=PORT,
+            user=USER,
+            passwd=PASSWORD,
+            database=DB_NAME,
+            cursorclass=pymysql.cursors.DictCursor
+        )
 
-connection = pymysql.connect(
-    host=HOST,
-    port=PORT,
-    user=USER,
-    passwd=PASSWORD,
-    database=DB_NAME,
-    cursorclass=pymysql.cursors.DictCursor
-)
+        self.cursor = self.connection.cursor()
+    def criar_tabela(self):
+            self.cursor.execute(f'CREATE TABLE IF NOT EXISTS {TABLE_NAME}(ID INTEGER PRIMARY KEY AUTO_INCREMENT, name TEXT, ap INTEGER)')
+            self.connection.commit()
 
-cursor = connection.cursor()
-def criar_tabela():
-    cursor.execute(f'CREATE TABLE IF NOT EXISTS {TABLE_NAME}(ID INTEGER PRIMARY KEY AUTO_INCREMENT, name TEXT, ap INTEGER)')
-    connection.commit()
+    def adicionar_morador(self, nome, ap):
+            sql_inserir = f'INSERT INTO {TABLE_NAME} (name, ap) VALUES (%s, %s)'
+            self.cursor.execute(sql_inserir, [nome, ap])
+            self.connection.commit()
 
-def adicionar_morador(nome, ap):
-    sql_inserir = f'INSERT INTO {TABLE_NAME} (name, ap) VALUES (%s, %s)'
-    cursor.execute(sql_inserir, [nome, ap])
-    connection.commit()
-
-def atualizar_morador(id, nome, ap):
-    sql_atualizar = f'UPDATE {TABLE_NAME} SET name=%s, ap=%s WHERE ID=%s'
-    cursor.execute(sql_atualizar, [nome, ap, id])
-    connection.commit()
-def visualizar():
-    cursor.execute(f'SELECT * FROM {TABLE_NAME}')
-    rows = cursor.fetchall()
-    if rows:
-        for row in rows:
-            print(f"ID: {row['ID']} Nome: {row['name']} AP: {row['ap']}")
-            print()
-    else:
-        print("Não há moradores cadastrados.")
+    def atualizar_morador(self, id, nome, ap):
+            sql_atualizar = f'UPDATE {TABLE_NAME} SET name=%s, ap=%s WHERE ID=%s'
+            self.cursor.execute(sql_atualizar, [nome, ap, id])
+            self.connection.commit()
+    def visualizar(self):
+            self.cursor.execute(f'SELECT * FROM {TABLE_NAME}')
+            rows = self.cursor.fetchall()
+            
+            if rows:
+                for row in rows:
+                    print()
+                    #print(f"ID: {row['ID']} Nome: {row['name']} AP: {row['ap']}")
+                    print(row)
+                    print()
+            else:
+                print("Não há moradores cadastrados.")
     
-def deletar_morador(id):
-    sql_delete = f'DELETE FROM {TABLE_NAME} WHERE id = %s'
-    cursor.execute(sql_delete, id)
-    connection.commit()
+    def deletar_morador(id):
+            sql_delete = f'DELETE FROM {TABLE_NAME} WHERE id = %s'
+            self.cursor.execute(sql_delete, id)
+            self.connection.commit()
 
-def fechar_conexao_com_banco():
-    cursor.close()
-    connection.close()
-    print("conexão encerrada...")
+    def fechar_conexao_com_banco():
+            self.cursor.close()
+            self.connection.close()
+            print("conexão encerrada...")
 
-criar_tabela()
-connection.commit()
+morador = Morador()
+morador.criar_tabela()
+#connection.commit()
 while True:
     print("digite (c) para criar morador \n(r) para ler lista de moradores \n(u) para atualizar \n(d) para deletar morador pelo id")
     opcao = input("digite a opção: ")
     if opcao == 'c':
         nome_morador = str(input("digite o nome: "))
         numero_ap = int(input("digite o numero do ap: "))
-        adicionar_morador(nome_morador, numero_ap)
+        morador.adicionar_morador(nome_morador, numero_ap)
         continue
     elif opcao == 'r':
-        visualizar()
+        morador.visualizar()
         continue
     elif opcao == 'u':
         print('atualizar pelo id')
         id_morador = int(input("digite o id: "))
         nome_morador = str(input("digite o nome: "))
         numero_ap = int(input("digite o numero do ap: "))
-        atualizar_morador(id_morador, nome_morador, numero_ap)
+        morador.atualizar_morador(id_morador, nome_morador, numero_ap)
 
         continue
     elif opcao =='d':
         id = int(input("digite o id para deletar: "))
-        deletar_morador(id)
+        morador.deletar_morador(id)
         continue
     elif opcao == 's':
         print("saindo...")
@@ -82,7 +88,7 @@ while True:
         print("opção inválida...")
         continue
 
-fechar_conexao_com_banco()
+morador.fechar_conexao_com_banco()
     
 
 
